8ae8ea05b2bd19195c0f4cec058435aef40fe05c
isaac-svg <isaacsakyi009@gmail.com>
2023-05-18 21:51:26 +0000

    commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,51 +1,81 @@
 import React from "react";
 import classes from "./main.module.css";
 import { useState } from "react";
+import proj4 from "proj4";
 const Main = () => {
-  const [WGS, setWGS] = useState("");
-  const [LEGION, setLEIGON] = useState("");
+  const [display, setDisplay] = useState([]);
   const [COORD, setCOORD] = useState("");
-  const convert = () => {};
+  const [type, setType] = useState("");
+  const [result, setResult] = useState(false);
+  const convert = (e) => {
+    e.preventDefault();
+    // check the type of conversion
+    if (type === "WGS to LEIGON") {
+      const coords = COORD.trim().split(",");
+      console.log(coords);
+      const long = coords[0].trim();
+      const lat = coords[1].trim();
+      console.log(long, lat);
+      setDisplay(
+        proj4("+proj=utm +zone=30 +ellps=WALLOFFICE +units=m +no_defs", [
+          Number(long),
+          Number(lat),
+        ])
+      );
+
+      setResult(true);
+    } else {
+      const coords = COORD.trim().split(",");
+      const long = coords[0].trim();
+      const lat = coords[1].trim();
+      setDisplay(
+        proj4(
+          "+proj=utm +zone=30 +ellps=WALLOFFICE +units=m +no_defs",
+          "EPSG:4326",
+          [Number(long), Number(lat)]
+        )
+      );
+
+      setResult(true);
+    }
+  };
   return (
     <div className={classes.main}>
       <div className={classes.mainWrapper}>
         <span className={classes.desc}>
           CONVERT WGS TO LEIGON AND VICE VERSA
         </span>
-        <div>
-          <label htmlFor="data" className={classes.upload}>
-            Upload a csv file
-          </label>
-          <input type="file" name="data" id="data" accept=".csv" hidden />
-        </div>
-        <form className={classes.form}>
-          <div className={classes.inputControl}>
-            <label htmlFor="WGS" className={classes.label}>
-              WGS
-            </label>
-            <input
-              type="text"
-              placeholder="WGS..."
-              id="WGS"
-              className={classes.input}
-            />
-          </div>
-          <div className={classes.inputControl}>
-            <label htmlFor="LEG" className={classes.label}>
-              LEIGON
-            </label>
-            <input
-              type="text"
-              placeholder="LEIGON..."
-              id="LEG"
-              className={classes.input}
-            />
+
+        <form className={classes.form} onSubmit={convert}>
+          <div className={classes.choose}>
+            {/* < className={classes.action}> */}
+
+            <div>
+              <select
+                className={classes.select}
+                value={type}
+                onChange={(e) => setType(e.target.value)}
+              >
+                <option value="Click to select">Click to select</option>
+                <option value="WGS to LEIGON">WGS to LEIGON</option>
+                <option value="LEIGON to WGS">LEIGON to WGS</option>
+              </select>
+            </div>
+            <div className={classes.convertWrapper}>
+              <button className={classes.convertBtn} type="submit">
+                Convert
+              </button>
+            </div>
           </div>
           <div className={classes.inputControl}>
             <label htmlFor="COORD" className={classes.label}>
               COORDINATES
             </label>
             <input
+              required
+              value={COORD}
+              onChange={(e) => setCOORD(e.target.value)}
+              title="comma seppareted coordinates"
               type="text"
               placeholder="e.g -122.305887, 58.9465872"
               id="COORD"
@@ -53,11 +83,26 @@ const Main = () => {
             />
           </div>
         </form>
-        {/* convert and upload */}
-
-        <div className={classes.convertWrapper}>
-          <button className={classes.convertBtn}>Convert</button>
-        </div>
+        {/* result */}
+        {result && (
+          <div className={classes.resultWrapper}>
+            {display.map((num, idx) => (
+              <span className={classes.display}>
+                {idx == 0 ? (
+                  <>
+                    LAT:
+                    <p>{num}</p> <br />
+                  </>
+                ) : (
+                  <>
+                    LONG:
+                    <p>{num}</p> <br />
+                  </>
+                )}
+              </span>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
